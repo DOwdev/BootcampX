dd563416f59dabbe6efc0b163e985c226628ea5f
DOwdev <63557295+DOwdev@users.noreply.github.com>
2021-05-06 21:03:42 -0400

    students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,19 @@ const pool = new Pool({
 });
 
 let cohort = process.argv[2];
-let limit = process.argv[3];
+let limit = process.argv[3] || 5;
 
-pool.query(`
+const values = [`%${cohort}%`, limit];
+
+const queryString =`
 SELECT students.id as id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
     res.rows.forEach(user => {
         console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`)
